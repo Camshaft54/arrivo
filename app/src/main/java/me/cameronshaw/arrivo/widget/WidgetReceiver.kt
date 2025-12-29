873a294489c81b84a9571e51f4a2c524e51a6899
Cameron Shaw <39676304+Camshaft54@users.noreply.github.com>
2025-12-28 23:28:12 -0600

    Improve robustness of refreshwidgetworker

--- app/src/main/java/me/cameronshaw/arrivo/widget/WidgetReceiver.kt
+++ app/src/main/java/me/cameronshaw/arrivo/widget/WidgetReceiver.kt
@@ -1,5 +1,6 @@
 package me.cameronshaw.arrivo.widget
 
+import android.appwidget.AppWidgetManager
 import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
@@ -15,11 +16,29 @@ import java.util.concurrent.TimeUnit
 class WidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = ArrivoWidget()
 
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        enqueueRefreshWork(context)
+    }
+
     override fun onEnabled(context: Context) {
         super.onEnabled(context)
+        enqueueRefreshWork(context)
+    }
 
-        val constraints =
-            Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+    override fun onDisabled(context: Context) {
+        super.onDisabled(context)
+        WorkManager.getInstance(context).cancelUniqueWork(RefreshWidgetWorker.UNIQUE_WORK_NAME)
+    }
+
+    private fun enqueueRefreshWork(context: Context) {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
 
         val periodicWorkRequest = PeriodicWorkRequestBuilder<RefreshWidgetWorker>(
             15, TimeUnit.MINUTES
@@ -27,14 +46,8 @@ class WidgetReceiver : GlanceAppWidgetReceiver() {
 
         WorkManager.getInstance(context).enqueueUniquePeriodicWork(
             RefreshWidgetWorker.UNIQUE_WORK_NAME,
-            ExistingPeriodicWorkPolicy.UPDATE,
+            ExistingPeriodicWorkPolicy.KEEP,
             periodicWorkRequest
         )
-    }
-
-    override fun onDisabled(context: Context) {
-        super.onDisabled(context)
-
-        WorkManager.getInstance(context).cancelUniqueWork(RefreshWidgetWorker.UNIQUE_WORK_NAME)
     }
 }
